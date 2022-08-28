42044b1ffbf76b8d042c3d94648f9262aa55465a
sajid365-sr <sajidsorker86@gmail.com>
2022-08-29 03:49:56 +0600

    individual meal section added

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ mealContainer.innerHTML ='';
         let mealDiv = document.createElement('div');
         mealDiv.classList.add('col');
         mealDiv.innerHTML =`
-        <div class="card">
+        <div class="card"  onclick ="loadMealDetails(${element.idMeal})">
                     <img src="${element.strMealThumb}" class="card-img-top" alt="...">
                     <div class="card-body">
                       <h4 class="card-title">${element.strMeal}</h4>
@@ -23,7 +23,7 @@ mealContainer.innerHTML ='';
                   </div>`;
 
                   mealContainer.appendChild(mealDiv)
-
+console.log(element)
                 
     });
 }
@@ -35,7 +35,45 @@ let searchFood = () =>{
     loadMeals(searchValue);
     inputField.value ='';
 }
+let loadMealDetails = (mealId) =>{
+
+let url= `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`
+    // console.log(mealId)
+    // console.log(url)
+
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayMealDetails(data.meals[0]))
+}
+
+let displayMealDetails = (mealDetail) =>{
+    
+    let detailContainer = document.getElementById('detail-container');
+
+    document.getElementById('hiddenMeal').className = 'd-block my-4 '
+   
+    detailContainer.innerHTML ='';
+
+
+
+let mealDiv = document.createElement('div');
+mealDiv.classList.add('card');
+mealDiv.innerHTML = `
+            <img src="${mealDetail.strMealThumb}" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">${mealDetail.strMeal}</h5>
+                  <p class="card-text">${mealDetail.strInstructions.slice(0,200)}</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+`
+detailContainer.appendChild(mealDiv)
+    
+
+}
+
 loadMeals('a');
+
+
 
 
 
